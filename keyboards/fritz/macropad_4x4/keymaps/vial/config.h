70b6b1cd4072218672a749ca1065f1abb6d11284
Fritz Koenig <frkoenig@gmail.com>
2025-09-09 19:29:32 -0700

    initial configuration

---
+++ keyboards/fritz/macropad_4x4/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x58, 0x55, 0xE7, 0x22, 0xE7, 0x2F, 0xD2, 0x9A}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
---
+++ keyboards/fritz/macropad_4x4/keymaps/vial/keymap.c
@@ -0,0 +1,32 @@
+// Copyright 2025 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │   │   │   │   │
+     * ├───┼───┼───├───┼
+     * │   │   │   │   │
+     * ├───┼───┼───┼───┤
+     * │   │   │   │   │
+     * ├───┼───┼───┤───│
+     * │   │   │   │   │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT_numpad_4x4(
+        KC_0,   KC_1,   KC_2,   KC_3,
+        KC_4,   KC_5,   KC_6,   KC_7,
+        KC_A,   KC_B,   KC_C,   KC_D,
+        KC_E,   KC_F,   KC_G,   KC_H
+    )
+};
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  debug_keyboard=true;
+  debug_mouse=true;
+}
