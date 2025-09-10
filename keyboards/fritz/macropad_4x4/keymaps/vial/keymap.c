86ffd9f083d2f6b0bdf033578064534a0388f627
Fritz Koenig <frkoenig@gmail.com>
2025-09-09 20:22:02 -0700

    checkpoint: vial can configure, but can't do rgb

--- keyboards/fritz/macropad_4x4/keymaps/vial/keymap.c
+++ keyboards/fritz/macropad_4x4/keymaps/vial/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │   │   │   │   │
      * └───┴───┴───┴───┘
      */
-    [0] = LAYOUT_numpad_4x4(
+    [0] = LAYOUT_ortho_4x4(
         KC_0,   KC_1,   KC_2,   KC_3,
         KC_4,   KC_5,   KC_6,   KC_7,
         KC_A,   KC_B,   KC_C,   KC_D,
