54cbf97e51d5f8f44c59b0385110bb59741287cf
Fritz Koenig <frkoenig@gmail.com>
2025-10-13 21:25:19 -0700

    update physical key positions

--- keyboards/fritz/rgb_advantage/keymaps/default/keymap.c
+++ keyboards/fritz/rgb_advantage/keymaps/default/keymap.c
@@ -9,6 +9,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,    KC_W,    KC_E,      KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,     KC_P,     KC_BSLS,
         KC_CAPS,  KC_A,    KC_S,    KC_D,      KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
         QK_BOOT,  KC_Z,    KC_X,    KC_C,      KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLASH, KC_RSFT,
-        KC_GRV,  KC_INS,  KC_LEFT,  KC_RIGHT,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_UP,   KC_DOWN,  KC_LBRC,  KC_RBRC
+        KC_GRV,  KC_INS,  KC_LEFT,  KC_RIGHT,                                  KC_UP,   KC_DOWN,  KC_LBRC,  KC_RBRC
     )
 };
