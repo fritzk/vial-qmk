d43a7779a80e65f747933a6110931b5bc2981b61
Fritz Koenig <frkoenig@gmail.com>
2025-11-02 17:27:18 -0800

    mimic mame buttons

--- keyboards/fritz/mame_stick/keymaps/default/keymap.c
+++ keyboards/fritz/mame_stick/keymaps/default/keymap.c
@@ -1,9 +1,19 @@
 #include QMK_KEYBOARD_H
 
+enum {
+    _P1 = 0,
+    _P2
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT(
-              KC_A,             KC_B, KC_C, KC_D, KC_E,
-        KC_F,       KC_G,       KC_H, KC_I, KC_J, KC_K,
-              KC_L
+  [_P1] = LAYOUT(
+                 KC_UP,               KC_LCTL, KC_LALT, KC_SPACE, KC_LSFT,
+        KC_LEFT,         KC_RIGHT,    KC_Z,    KC_X,    KC_C,     KC_V,
+                 KC_DOWN
   ),
+  [_P2] = LAYOUT(
+                 KC_R,                KC_Q,    KC_W,    KC_E,     KC_T,
+        KC_D,            KC_G,        KC_A,    KC_S,    KC_V,     KC_B,
+                 KC_F
+  )
 };
