f43731f1825c86e89848410220d4921ff1d198b0
Fritz Koenig <frkoenig@gmail.com>
2025-11-03 21:25:33 -0800

    remove unused functionality

--- keyboards/fritz/macropad_4x4/keymaps/vial/keymap.c
+++ keyboards/fritz/macropad_4x4/keymaps/vial/keymap.c
@@ -22,11 +22,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_E,   KC_F,   KC_G,   KC_H
     )
 };
-
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-  debug_keyboard=true;
-  debug_mouse=true;
-}
