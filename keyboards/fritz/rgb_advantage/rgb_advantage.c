a3d9b6cb90d18d6413fe33eb27eba878c2cfd4bd
Fritz Koenig <frkoenig@gmail.com>
2025-11-09 14:31:42 -0800

    fix up key/led map

--- keyboards/fritz/rgb_advantage/rgb_advantage.c
+++ keyboards/fritz/rgb_advantage/rgb_advantage.c
@@ -4,11 +4,12 @@
 led_config_t g_led_config = { {
     // Key Matrix to LED Index
     { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-    { NO_LED, NO_LED,     33,     32,     31,     30,     29,     28,      0,      1,      2,      3,      4,      5,},
-    { NO_LED, NO_LED,     34,     35,     36,     37,     38,     39,     11,     10,      9,      8,      7,      6 },
-    { NO_LED, NO_LED,     45,     44,     43,     42,     41,     40,     12,     13,     14,     15,     16,     17 },
-    { NO_LED, NO_LED,     46,     47,     48,     49,     50,     51,     23,     22,     21,     20,     19,     18 },
-    { NO_LED, NO_LED,     55,     54,     53,     52, NO_LED, NO_LED, NO_LED, NO_LED,     24,     25,     26,     27 },
+    {     31,     30,     45,     44,     43,     42,     41,     40,      0,      1,      2,      3,      4,      5,},
+    {     32,     38,     46,     47,     48,     49,     50,     51,     11,     10,      9,      8,      7,      6 },
+    {     33,     29,     57,     56,     55,     54,     53,     52,     12,     13,     14,     15,     16,     17 },
+    {     35,     37,     58,     59,     60,     61,     62,     63,     23,     22,     21,     20,     19,     18 },
+    {     34,     39,     64,     65,     66,     67, NO_LED, NO_LED, NO_LED, NO_LED,     24,     25,     26,     27 },
+    {     36,     28, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
     { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
 }, {
     // LED Index to Physical Position
@@ -50,45 +51,14 @@ led_config_t g_led_config = { {
     { 200,  39 }, // [
     { 212,  43 }, // ]
 
-    // 28
-    {  55,  1 }, // 5
-    {  46,  1 }, // 4
-    {  36,  0 }, // 3
-    {  25,  0 }, // 2
-    {  12,  3 }, // 1
-    {  0,   2 }, // =
+    // right
+    { 166, 52 }, // space
+    { 150, 41 }, // right ctrl
+    { 156, 56 }, // enter
+    { 140, 45 }, // super
+    { 144, 55 }, // page up
+    { 148, 64 }, // page down
 
-    // 34
-    {   1, 11 }, // tab
-    {  13, 13 }, // q
-    {  26, 10 }, // w
-    {  38, 10 }, // e
-    {  48, 10 }, // r
-    {  58, 11 }, // t
-
-    // 40
-    {  58, 21 }, // g
-    {  48, 20 }, // f
-    {  39, 20 }, // d
-    {  26, 20 }, // s
-    {  14, 23 }, // a
-    {   1, 21 }, // caps
-
-    // 46
-    {   0, 31 }, // left shift
-    {  13, 33 }, // z
-    {  26, 30 }, // x
-    {  38, 30 }, // c
-    {  48, 30 }, // v
-    {  58, 31 }, // b
-
-    // 52
-    {  46, 40 }, // right arrow
-    {  36, 39 }, // left arrow
-    {  23, 39 }, // |
-    {  12, 43 }, // `
-/*
-    // TODO: thumb clusters
     // left
     {  58, 52 }, // backspace
     {  74, 41 }, // left ctrl
@@ -97,14 +67,43 @@ led_config_t g_led_config = { {
     {  80, 55 }, // home
     {  76, 64 }, // end
 
-    // right
-    { 166, 52 }, // space
-    { 150, 41 }, // right ctrl
-    { 156, 56 }, // enter
-    { 140, 45 }, // super
-    { 144, 55 }, // page up
-    { 148, 64 }, // page down
-*/
+    // 40
+    {  55,  1 }, // 5
+    {  46,  1 }, // 4
+    {  36,  0 }, // 3
+    {  25,  0 }, // 2
+    {  12,  3 }, // 1
+    {  0,   2 }, // =
+
+    // 46
+    {   1, 11 }, // tab
+    {  13, 13 }, // q
+    {  26, 10 }, // w
+    {  38, 10 }, // e
+    {  48, 10 }, // r
+    {  58, 11 }, // t
+
+    // 52
+    {  58, 21 }, // g
+    {  48, 20 }, // f
+    {  39, 20 }, // d
+    {  26, 20 }, // s
+    {  14, 23 }, // a
+    {   1, 21 }, // caps
+
+    // 58
+    {   0, 31 }, // left shift
+    {  13, 33 }, // z
+    {  26, 30 }, // x
+    {  38, 30 }, // c
+    {  48, 30 }, // v
+    {  58, 31 }, // b
+
+    // 64
+    {  46, 40 }, // right arrow
+    {  36, 39 }, // left arrow
+    {  23, 39 }, // |
+    {  12, 43 }, // `
 }, {
     // LED Index to Flag
     LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
@@ -115,5 +114,7 @@ led_config_t g_led_config = { {
     LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
     LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
     LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+    LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+    LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
 } };
 #endif
