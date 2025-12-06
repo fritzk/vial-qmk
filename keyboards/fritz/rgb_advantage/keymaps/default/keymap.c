7f2ed0125b0e1c44d6bbb59d338bfe3d95a1df26
Fritz Koenig <frkoenig@gmail.com>
2025-12-05 22:34:21 -0800

    use KC_TRNS instead of KC_NO on layers

--- keyboards/fritz/rgb_advantage/keymaps/default/keymap.c
+++ keyboards/fritz/rgb_advantage/keymaps/default/keymap.c
@@ -3,11 +3,14 @@
 
 #include QMK_KEYBOARD_H
 
+// clang-format off
 enum layer_names {
   _QWERTY = 0,
   _MOUSE
 };
+// clang-format on
 
+// clang-format off
 /****************************************************************************************************
 *
 * Keymap: Default Layer in Qwerty
@@ -48,24 +51,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_MOUSE] = LAYOUT(
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  TO(_QWERTY),    QK_BOOT,
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                                                                      KC_NO,     KC_NO,    KC_NO,    KC_NO,  KC_NO,      KC_NO,
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                                                                      KC_NO,   KC_ACL0,  KC_ACL1, KC_ACL2,   KC_NO,      KC_NO,
-    KC_NO,    KC_NO,  MS_BTN1,  MS_BTN3,  MS_BTN2,    KC_NO,                                                                    MS_LEFT,   MS_DOWN,    MS_UP,  MS_RGHT,  KC_NO,      KC_NO,
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                                                                      KC_NO,     KC_NO,    KC_NO,    KC_NO,  KC_NO,      KC_NO,
-              KC_NO,    KC_NO,    KC_NO,    KC_NO,                                                                                           KC_NO,    KC_NO,    KC_NO,  KC_NO,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                                                                   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                                                                   KC_TRNS,  KC_ACL0,  KC_ACL1,  KC_ACL2,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  MS_BTN1,  MS_BTN3,  MS_BTN2,  KC_TRNS,                                                                   MS_LEFT,  MS_DOWN,  MS_UP,    MS_RGHT,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                                                                   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+              KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                                                                                       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
                                                       KC_LCTL,  KC_LALT,                                               KC_RGUI,  KC_RCTL,
                                                                 KC_HOME,                                               KC_PGUP,
                                             KC_BSPC,  KC_DEL,   KC_END,                                                KC_PGDN, KC_ENTER, KC_SPC
   )
 };
+// clang-format on
 
 #ifdef OLED_ENABLE
 static void render_logo(void) {
+    // clang-format off
     static const char PROGMEM qmk_logo[] = {
         0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
         0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
         0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
     };
+    // clang-format on
 
     oled_write_P(qmk_logo, false);
 }
